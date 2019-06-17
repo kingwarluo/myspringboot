a59817759c70464798249e65e5f135c4fe7ceecb
罗建华 <jianhua.luo@ucarinc.com>
2019-06-17 09:17:27 +0800

    缓存测试阶段--整合redis

---
+++ src/main/java/com/kingwarluo/myspringboot/common/cache/MyRedisConfig.java
@@ -0,0 +1,40 @@
+package com.kingwarluo.myspringboot.common.cache;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @description:redis配置
+ *
+ * @author jianhua.luo
+ * @date 2019/6/15
+ */
+@Configuration
+public class MyRedisConfig {
+
+    @Bean("redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        //设置各种数据类型的序列化方式
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(keySerializer());
+        redisTemplate.setHashKeySerializer(keySerializer());
+        redisTemplate.setValueSerializer(valueSerializer());
+        redisTemplate.setHashValueSerializer(valueSerializer());
+        return redisTemplate;
+    }
+
+    private RedisSerializer<String> keySerializer(){
+        return new StringRedisSerializer();
+    }
+
+    private RedisSerializer<Object> valueSerializer(){
+        return new GenericJackson2JsonRedisSerializer();
+    }
+
+}
