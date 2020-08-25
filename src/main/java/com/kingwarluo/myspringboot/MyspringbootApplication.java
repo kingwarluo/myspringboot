824343dba52b71dd1a340333c1e427c79665a122
罗建华 <jianhua.luo@ucarinc.com>
2020-08-25 14:07:51 +0800

    mybatis

--- src/main/java/com/kingwarluo/myspringboot/MyspringbootApplication.java
+++ src/main/java/com/kingwarluo/myspringboot/MyspringbootApplication.java
@@ -1,10 +1,7 @@
 package com.kingwarluo.myspringboot;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
 /**
  * @description:应用启动类
@@ -12,9 +9,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerA
  * @author jianhua.luo
  * @date 2019/6/15
  */
-@SpringBootApplication(
-    exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class}
-)
+@SpringBootApplication
 public class MyspringbootApplication {
 
     public static void main(String[] args) {
